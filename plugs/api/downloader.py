b9fa7b9fb53bd1f7c73358f7657d10a971626742
0×ALPHA <68905945+noobALPHA@users.noreply.github.com>
2023-06-12 16:17:36 +0530

    Update downloader.py

--- plugs/api/downloader.py
+++ plugs/api/downloader.py
@@ -9,25 +9,25 @@ from pyrogram.types import Message
 )
 async def pin_dl(client, msg: Message) -> Message:
     url = f"https://{msg.matches[0].group(1)}"
-    msg_tmp: Message = await msg.reply(waiting_text, quote=True)
+    msg_tmp: Message = await msg.reply_text(waiting_text, quote=True)
     
     dl = download(url)
     if dl:
         send_type, url = dl
-        await msg_tmp.edit("**__Uploading to telegram__**")
+        await msg_tmp.edit_text("**__Uploading to telegram__**")
         if send_type == "gif":
-            await msg.reply_animation(url, caption=caption, reply_to_message_id=msg.message_id)
+            await client.send_animation(msg.chat.id, animation=url, caption=caption, reply_to_message_id=msg.message_id)
         
         elif send_type == "video":
-            await msg.reply_video(url, caption=caption, reply_to_message_id=msg.message_id)
+            await client.send_video(msg.chat.id, video=url, caption=caption, reply_to_message_id=msg.message_id)
         
         elif send_type == "image":
-            await msg.reply_photo(url, caption=caption, reply_to_message_id=msg.message_id)
+            await client.send_photo(msg.chat.id, photo=url, caption=caption, reply_to_message_id=msg.message_id)
         
-        return await msg_tmp.delete()
+        await msg_tmp.delete()
             
     else:
-        return await msg.reply_text(error)
+        await msg.reply_text(error)
 
 
 @Client.on_edited_message(
@@ -35,22 +35,22 @@ async def pin_dl(client, msg: Message) -> Message:
 )
 async def pin_dl_edited(client, msg: Message) -> Message:
     url = f"https://{msg.matches[0].group(1)}"
-    msg_tmp: Message = await msg.reply(waiting_text, quote=True)
+    msg_tmp: Message = await msg.reply_text(waiting_text, quote=True)
     
     dl = download(url)
     if dl:
         send_type, url = dl
-        await msg_tmp.edit("**__Uploading to telegram__**")
+        await msg_tmp.edit_text("**__Uploading to telegram__**")
         if send_type == "gif":
-            await msg.reply_animation(url, caption=caption, reply_to_message_id=msg.message_id)
+            await client.send_animation(msg.chat.id, animation=url, caption=caption, reply_to_message_id=msg.message_id)
         
         elif send_type == "video":
-            await msg.reply_video(url, caption=caption, reply_to_message_id=msg.message_id)
+            await client.send_video(msg.chat.id, video=url, caption=caption, reply_to_message_id=msg.message_id)
         
         elif send_type == "image":
-            await msg.reply_photo(url, caption=caption, reply_to_message_id=msg.message_id)
+            await client.send_photo(msg.chat.id, photo=url, caption=caption, reply_to_message_id=msg.message_id)
         
-        return await msg_tmp.delete()
+        await msg_tmp.delete()
             
     else:
-        return await msg.reply_text(error)
+        await msg.reply_text(error)
