25eb5459ab725af2db478772accea9bee683f708
0×ALPHA <68905945+noobALPHA@users.noreply.github.com>
2023-06-12 16:00:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from pyrogram import Client
 
-api_id = 0
-api_hash = ""
-bot_token = ""
+api_id = 2332973
+api_hash = "157025425ad2637898b4710e019f809f"
+bot_token = "5768890343:AAFKO0BPEPPzSJjJ2OFNRyhaZQtfASkMPss"
 
 app = Client(
     "bot",
