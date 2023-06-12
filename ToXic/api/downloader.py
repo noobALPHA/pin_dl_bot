7f8c75d8295d6f09a048c27e4566881d793c0fbb
0×ALPHA <68905945+noobALPHA@users.noreply.github.com>
2023-06-12 21:11:29 +0530

    Update downloader.py

--- ToXic/api/downloader.py
+++ ToXic/api/downloader.py
@@ -3,7 +3,6 @@ from ToXic.texts import caption, error, waiting_text
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-OWNER_ID=int(1057412250)
 @Client.on_message(
     filters.regex(r"(pinterest\.com/pin/[^/]+|pin\.it/[^/]+)(/$|$)")
 )
@@ -22,7 +21,7 @@ async def pin_dl(client, msg: Message) -> Message:
                     InlineKeyboardButton(text="✨ sᴜᴩᴩᴏʀᴛ ✨", url="https://ChatHuB_x_D.t.me"),
                 ],
                 [
-                    InlineKeyboardButton("🥀ᴅᴇᴠᴇʟᴏᴘᴇʀ🥀", user.id=OWNER_ID)
+                    InlineKeyboardButton("🥀ᴅᴇᴠᴇʟᴏᴘᴇʀ🥀", user_id=1057412250)
                 ]
             ]
         )
@@ -40,40 +39,4 @@ async def pin_dl(client, msg: Message) -> Message:
             
     else:
         return await msg.reply_text(error)
-
-
-# @Client.on_edited_message(
-#     filters.regex(r"(pinterest\.com/pin/[^/]+|pin\.it/[^/]+)(/$|$)")
-# )
-# async def pin_dl_edited(client, msg: Message) -> Message:
-#     url = f"https://{msg.matches[0].group(1)}"
-#     msg_tmp: Message = await msg.reply(waiting_text, quote=True)
     
-#     dl = download(url)
-#     if dl:
-#         send_type, url = dl
-#         await msg_tmp.edit("**__Uploading to telegram__**")
-#         buttons = InlineKeyboardMarkup(
-#             [
-#                  [
-#                     InlineKeyboardButton(text="❄ ᴄʜᴀɴɴᴇʟ ❄", url="https://NOOBSHEAVEN.t.me"),
-#                     InlineKeyboardButton(text="✨ sᴜᴩᴩᴏʀᴛ ✨", url="https://ChatHuB_x_D.t.me"),
-#                 ],
-#                 [
-#                     InlineKeyboardButton("🥀ᴅᴇᴠᴇʟᴏᴘᴇʀ🥀", user.id=1057412250)
-#                 ]
-#             ]
-#         )
-#         if send_type == "gif":
-#             await msg.reply_animation(url, caption=caption, reply_to_message_id=msg.id, reply_markup=buttons)
-        
-#         elif send_type == "video":
-#             await msg.reply_video(url, caption=caption, reply_to_message_id=msg.id, reply_markup=buttons)
-        
-#         elif send_type == "image":
-#             await msg.reply_photo(url, caption=caption, reply_to_message_id=msg.id, reply_markup=buttons)
-        
-#         return await msg_tmp.delete()
-            
-#     else:
-#         return await msg.reply_text(error)
