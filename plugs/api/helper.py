a4e1dc9673d112e0e1a2668c0710971c43a64801
0×ALPHA <68905945+noobALPHA@users.noreply.github.com>
2023-06-12 16:04:32 +0530

    Update helper.py

--- plugs/api/helper.py
+++ plugs/api/helper.py
@@ -3,10 +3,11 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 
-@Client.on_message(
-    ~filters.edited &
-    filters.command("help")
-)
+@Client.on_message(filters.command("help"))
+async def helper(client: Client, message: Message) -> Message:
+    return await message.reply(help_text, disable_web_page_preview=True)
 
-async def helper(_, msg:Message) -> Message:
-    return await msg.reply(help_text, disable_web_page_preview=True)
+
+@Client.on_edited_message(filters.command("help"))
+async def edited_helper(client: Client, message: Message) -> Message:
+    return await message.reply(help_text, disable_web_page_preview=True)
